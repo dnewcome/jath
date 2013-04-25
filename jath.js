c48b61a048aa5c622e4d701fd1fbf69cfd1236a1
Dan Newcome <dan@dnuke.com>
2013-04-24 22:27:07 -0700

    removed spurious log statement from node.js code path

--- jath.js
+++ jath.js
@@ -115,15 +115,14 @@ function parseItem( template, xmldoc, node ) {
 		}
 	}
 	else if( m_browser == 'node' ) {
-		require('util').puts( template );	
-			// node can be null if query fails
-			var itemNode = node.get( template );
-			if( itemNode ) {
-				return itemNode.text();
-			}
-			else {
-				return null;
-			}
+		// node can be null if query fails
+		var itemNode = node.get( template );
+		if( itemNode ) {
+			return itemNode.text();
+		}
+		else {
+			return null;
+		}
 	}
 	else {
 		if( typeOf( template ) == 'string' && template[0] != Jath.literalChar ) {
