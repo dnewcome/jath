81ce0bb9359fbf94d6ea77ec9e0d29720a7065e4
Dan Newcome <dan@dnuke.com>
2013-04-24 22:27:38 -0700

    added html hcard parsing test for nodejs/libxmljs

--- jathnodetest.js
+++ jathnodetest.js
@@ -8,10 +8,17 @@ var fs = require('fs');
 var xml = require('libxmljs');
 var util = require('util');
 var jath = require('./jath');
-var template = [ "//label", { id: "@id", added: "@added" } ];
+var html_template = [ "//li", { id: "@id", added: "@added" } ];
+var xml_template = [ "//label", { id: "@id", added: "@added" } ];
 
 fs.readFile('labels.xml', 'ascii', function(err, data) {
   var xmlDoc = xml.parseXmlString(data);
-  var result = jath.parse(template, xmlDoc);
+  var result = jath.parse(xml_template, xmlDoc);
+  util.puts(util.inspect(result, false, 10));
+});
+
+fs.readFile('labels.html', 'ascii', function(err, data) {
+  var htmlDoc = xml.parseHtmlString(data);
+  var result = jath.parse(html_template, htmlDoc);
   util.puts(util.inspect(result, false, 10));
 });
