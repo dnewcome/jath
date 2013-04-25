38893a1fcdca360bc5637db238148f2e70076727
Dan Newcome <dan@dnuke.com>
2013-04-25 01:48:31 -0700

    cleaned up directory structure, moved tests

--- tests/windows-scripting/wsh-test.js
+++ tests/windows-scripting/wsh-test.js
@@ -3,7 +3,7 @@ var xmlDoc = new ActiveXObject("MSXML2.DOMDocument.6.0");
 xmlDoc.async = false;
 xmlDoc.setProperty( "SelectionLanguage", "XPath" );
 xmlDoc.resolveExternals = false;
-xmlDoc.load( "labels.xml" );
+xmlDoc.load( "..\labels.xml" );
 
 var template = [ "//label", { id: "@id", added: ":added" } ];
 var result = Jath.parse( template, xmlDoc );
